a9adbbc1e0a66d990ca1d23969b452d04fa357eb
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-11-18 12:53:27 +0545

    small changes on logout section

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,24 @@
 import CreateTeam from "../components/CreateTeam";
 import CreatePlayer from "../components/CreatePlayer";
+import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
+  const { logout } = useAuth();
+
   return (
     <div>
       <div className="p-4">
         <h1 className="text-lg font-bold mb-4">Admin Dashboard</h1>
         <CreatePlayer />
         <CreateTeam />
+        <div>
+          <button
+            onClick={logout}
+            className="bg-red-600 hover:bg-red-700 text-white font-bold px-6 py-2  transition"
+          >
+            LOGOUT
+          </button>
+        </div>
       </div>
     </div>
   );
